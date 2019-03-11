dcb7d8f5a21022949b68604de9c912abcadb52c7
Vectormike40 <victor.junior40@yahoo.com>
2019-03-11 06:23:42 +0100

    Added three components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import Title from './components/Title/Title';
+import Form from './components/Form/Form';
+import Weather from './components/Weather/Weather';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
+        <Title/>
+        <Form/>
+        <Weather/>
       </div>
     );
   }
---
+++ src/components/Weather/Weather.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Weather = () => {
+    return (
+        <div>Weather Component</div>
+    )
+}
+
+export default Weather;
