daed577d348bbbf72277c20c1f48f93ff6f5a64d
Vectormike40 <victor.junior40@yahoo.com>
2019-03-23 22:42:19 +0100

    More styling

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,18 +2,33 @@ import React from 'react';
 
 const Form = ({onInputChange, onButtonSubmit}) => {
     return (
-        <div>
+        <div className="pa4-l">
+            <form className="bg-light-red mw7 center pa4 br2-ns ba b--black-10">
+            <fieldset class="cf bn ma0 pa0">
+            <legend class="pa0 f5 f4-ns mb3 black-80">Type in a location</legend>
+            <div className="cf">
             <input 
+            className="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns"
             type='text' 
             name='city' 
-            placeholder='City'
+            placeholder='City or Country'
             onChange={onInputChange}
             />
             <button 
+            className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns"
             type='submit'
             onClick={onButtonSubmit}
-            >Search
+            > Search
             </button>
+            </div>
+            </fieldset>
+            </form>
+
+ 
+
+
+ 
+            
         </div>
     )
 } 
