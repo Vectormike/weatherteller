1ef56965cd4fcec41a9a5cea0229c2721244cfa6
Vectormike40 <victor.junior40@yahoo.com>
2019-03-24 07:10:41 +0100

    ...

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Form = ({onInputChange, onButtonSubmit}) => {
     return (
-        <div className="pa4-l">
-            <div className="bg-light-red mw7 center pa4 br2-ns ba b--black-10">
+        <div className="pa4">
+            <div className="shadow-5 bg-light-red mw7 center pa4 br2-ns ba b--black-10">
             <fieldset className="cf bn ma0 pa0">
             <legend class="pa0 f5 f4-ns mb3 black-80">Type in a location</legend>
             <div className="cf">
@@ -15,7 +15,7 @@ const Form = ({onInputChange, onButtonSubmit}) => {
             onChange={onInputChange}
             />
             <button 
-            className="f6 f5-l fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns"
+            className="grow f6 f5-l fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns"
             type='submit'
             onClick={onButtonSubmit}
             > Search
