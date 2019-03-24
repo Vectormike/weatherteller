666074eaa6f55bc506764acc3f6580a9cbb923ad
Vectormike40 <victor.junior40@yahoo.com>
2019-03-24 06:45:21 +0100

    ...

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -5,6 +5,7 @@ const Weather = ({icon, weather, degree, country, city, description}) => {
     return (
         <div>
             <img src={`http://openweathermap.org/img/w/${icon}.png`} alt={`Weather Icon`}/>
+            <div class="bt"> </div>
             <p>{weather}</p>
             <p>{degree} in {city}, {country}</p>
             <p>{description}</p>
