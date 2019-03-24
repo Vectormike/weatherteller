42d0232f83a731c933ce72f58275c5276b73c7ee
Vectormike40 <victor.junior40@yahoo.com>
2019-03-24 18:27:15 +0100

    ...

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Form = ({onInputChange, onButtonSubmit}) => {
     return (
-        <div className="pa4">
-            <div className="shadow-5 bg-light-blue mw7 center pa4 br2">
+        <div className="pa3">
+            <div style={{
+                backgroundColor: '#EA9984'
+
+            }} 
+            className="shadow-5 bg-gray mw7 center pa4 br3">
                 <fieldset className="cf bn ma0 pa0">
                     <div className="cf">
                         <input 
@@ -14,7 +18,7 @@ const Form = ({onInputChange, onButtonSubmit}) => {
                         onChange={onInputChange}
                         />
                         <button 
-                        className="grow f6 f5-l fl pv3 tc bn bg-animate bg-black-70 white w-20-l br2 br--right-ns"
+                        className="grow f6 f5-l fl pv3 tc bn bg-near-black white w-20-l br2 br--right-ns"
                         type='submit'
                         onClick={onButtonSubmit}
                         > Search
