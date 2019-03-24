a499b6695d316595422a65227081eeabfb4af327
Vectormike40 <victor.junior40@yahoo.com>
2019-03-24 06:57:42 +0100

    ...

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -2,10 +2,11 @@ import React from 'react'
 import './Weather.scss';
 
 const Weather = ({icon, weather, degree, country, city, description}) => {
+    
     return (
         <div>
             <img src={`http://openweathermap.org/img/w/${icon}.png`} alt={`Weather Icon`}/>
-            <div class="bt"> </div>
+            
             <p>{weather}</p>
             <p>{degree} in {city}, {country}</p>
             <p>{description}</p>
