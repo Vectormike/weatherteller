3f82fabdda7147b49c22449adc822e60dc024fde
Vectormike40 <victor.junior40@yahoo.com>
2019-05-29 14:50:23 +0100

    Updates

---
+++ precache-manifest.fb4ce9c1f1d57d1012b505523a428779.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0602bed712b677c7d0e1f8407128ffe8",
+    "url": "/weatherteller/index.html"
+  },
+  {
+    "revision": "f8051a1b47bdb9831c14",
+    "url": "/weatherteller/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "32e452e82c88c3ff7e67",
+    "url": "/weatherteller/static/css/main.e78b7059.chunk.css"
+  },
+  {
+    "revision": "f8051a1b47bdb9831c14",
+    "url": "/weatherteller/static/js/2.d4aeacc4.chunk.js"
+  },
+  {
+    "revision": "32e452e82c88c3ff7e67",
+    "url": "/weatherteller/static/js/main.e5fde0d8.chunk.js"
+  },
+  {
+    "revision": "d52a38e026d7c801b726",
+    "url": "/weatherteller/static/js/runtime~main.ac3cbe8f.js"
+  }
+]);
